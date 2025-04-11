1cb706056c4d556e3c257a3dfe5451ca26b21f9b
Fareed R <fareed1983@gmail.com>
2025-04-11 22:04:53 +1000

    updated index.html

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -5,7 +5,7 @@ mkdir -p output
 for md_file in $(find site -name "*.md"); do
 	html_file="output/$(basename ${md_file%.md}.html)"
 	echo Generating $html_file from $md_file...
-	sed 's/\.md/\.html/g' "$md_file" | pandoc -s -o "$html_file" --metadata title="Fareed R Digital Profile"
+	sed 's/\.md/\.html/g' "$md_file" | pandoc --from markdown+raw_html -s -o "$html_file" --metadata title="Fareed R Digital Profile"
 done
 
 echo Copying directories...
